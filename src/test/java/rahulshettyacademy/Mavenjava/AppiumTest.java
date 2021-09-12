f104b1800d5672e70eb072d9373f74cbf5f7acbf
abhinash <abhinashdesu@gmail.com>
2021-09-12 14:04:03 +0530

    first commit

---
+++ src/test/java/rahulshettyacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,21 @@
+package rahulshettyacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+	
+
+}
---
+++ src/test/java/rahulshettyacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,18 @@
+package rahulshettyacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowseAutomantion()
+	{
+		System.out.println("BrowseAutomantion");
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");
+	}
+}
